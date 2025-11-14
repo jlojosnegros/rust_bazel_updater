a24de092c230083602c96f9a6f9bc3517531d982
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2025-11-14 11:41:22 +0100

    just playing with subcommands
    Signed-off-by: Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+mod commands;
+
 use clap::{Arg, Command};
+use dotenv::dotenv;
 
 fn main() -> anyhow::Result<()> {
-    let command = Command::new("Bazel updater application")
+    dotenv().ok();
+
+    let mut command = Command::new("Bazel updater application")
         .version("0.1")
         .author("Jose Luis Ojosnegros Manchon <jl.ojosnegros.manchon@gmail.com>")
         .about("Simple application to create/update dependencies in bazel repository_locations.bzl files")
@@ -12,7 +17,14 @@ fn main() -> anyhow::Result<()> {
                .help("repository_locations.bzl file location"),
         );
 
+    // configure subcommands
+    command = commands::configure(command);
+
+    // get matches
     let matches = command.get_matches();
+
+    // handle matches
+    commands::handle(&matches)?;
 
     Ok(())
 }
