f7bea4ff58b2dfcf9185cd77abe71736d5770564
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2025-11-14 11:26:27 +0100

    very basic command in main
    Signed-off-by: Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Arg, Command};
+
+fn main() -> anyhow::Result<()> {
+    let command = Command::new("Bazel updater application")
+        .version("0.1")
+        .author("Jose Luis Ojosnegros Manchon <jl.ojosnegros.manchon@gmail.com>")
+        .about("Simple application to create/update dependencies in bazel repository_locations.bzl files")
+        .arg(
+            Arg::new("repo_loc")
+               .short('f')
+               .long("repo_loc_file")
+               .help("repository_locations.bzl file location"),
+        );
+
+    let matches = command.get_matches();
+
+    Ok(())
 }
